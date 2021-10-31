9fe8eeed90cae183c7529ab09b5f7fbb7ae381a6
Omm-Rajesh <91077035+Omm-Rajesh@users.noreply.github.com>
2021-10-31 11:42:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://youtu.be/O4GcFu8z3k0");
+  ifrm.setAttribute("src", "https://drive.google.com/file/d/15C5w4nvcrmGF2EzQOEoFo3g_FpV65yQx/view?usp=sharing");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
